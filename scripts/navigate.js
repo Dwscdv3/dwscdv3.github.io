7e9331315e490809386dcde49e77e778ca99e26e
Dwscdv3 <dwscdv3@hotmail.com>
2016-12-07 17:39:02 +0800

    移除：startsWith

--- scripts/navigate.js
+++ scripts/navigate.js
@@ -7,14 +7,12 @@ document.addEventListener("DOMContentLoaded", navigateToArticle);
 window.addEventListener("hashchange", navigateToArticle);
 
 function navigateToArticle() {
-    setTimeout(function() {
-        if (window.location.hash.length <= 1) {
-            ajaxGet("/articles/about", renderMarkdown);
-        }
-        if (window.location.hash.startsWith(prefixes.articles)) {
-            ajaxGet(window.location.hash.substring(1), renderMarkdown);
-        }
-    }, 0);
+    if (window.location.hash.length <= 1) {
+        ajaxGet("/articles/about", renderMarkdown);
+    }
+    else {
+        ajaxGet(window.location.hash.substring(1), renderMarkdown);
+    }
 }
 
 function renderMarkdown() {
