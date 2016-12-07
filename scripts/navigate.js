57958aa1fe067226e15f48c0d2d71aaf08b59e2e
Dwscdv3 <dwscdv3@hotmail.com>
2016-12-07 16:41:46 +0800

    重命名：删除 .md 后缀

--- scripts/navigate.js
+++ scripts/navigate.js
@@ -9,22 +9,18 @@ window.addEventListener("hashchange", navigateToArticle);
 function navigateToArticle() {
     setTimeout(function() {
         if (window.location.hash.length <= 1) {
-            ajaxGet("/articles/about.md", function() {
-                if (this.readyState == XMLHttpRequest.DONE) {
-                    if (this.status == 200) {
-                        $("#main").innerHTML = md.render(this.responseText);
-                    }
-                }
-            });
+            ajaxGet("/articles/about", renderMarkdown);
         }
         if (window.location.hash.startsWith(prefixes.articles)) {
-            ajaxGet(window.location.hash.substring(1) + ".md", function() {
-                if (this.readyState == XMLHttpRequest.DONE) {
-                    if (this.status == 200) {
-                        $("#main").innerHTML = md.render(this.responseText);
-                    }
-                }
-            });
+            ajaxGet(window.location.hash.substring(1), renderMarkdown);
         }
     }, 0);
+}
+
+function renderMarkdown() {
+    if (this.readyState == XMLHttpRequest.DONE) {
+        if (this.status == 200) {
+            $("#main").innerHTML = md.render(this.responseText);
+        }
+    }
 }
