a0b060bf45e5181080d8670c3c79557a1372f90b
Dwscdv3 <dwscdv3@hotmail.com>
2017-01-21 23:20:25 +0800

    + formatAlignedDate

--- scripts/common.js
+++ scripts/common.js
@@ -13,6 +13,26 @@ function min(a, b) {
     return a > b ? b : a;
 }
 
+function formatAlignedDate(date, separator, largeSeparator) {
+    if (date instanceof Date) {
+        if (!separator) {
+            separator = " ";
+        }
+        if (!largeSeparator) {
+            largeSeparator = separator + " ";
+        }
+        var dateString = "";
+        dateString += date.getUTCFullYear();
+        dateString += date.getUTCMonth() < 10 ? largeSeparator : separator;
+        dateString += date.getUTCMonth();
+        dateString += date.getUTCDay() < 10 ? largeSeparator : separator;
+        dateString += date.getUTCDay();
+        return dateString;
+    } else {
+        throw "Illegal param type: Date expected";
+    }
+}
+
 function Then(func) {
     var newArgs = new Array(arguments.length - 1);
     for (var i = 1; i < arguments.length; i++) {
